20eff3730f5564fbcf02f92a18a25ffa52e9b6b4
Jiaya <gaia.l.balossi@gmail.com>
2024-12-01 14:52:37 +0100

    Update script.js
    changed colours options + added event listeners for other buttons

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Select the button and the body
 const changeColorBtn = document.getElementById('changeColorBtn');
+const chooseOwnList = document.getElementById('chooseOwnList');
+const chooseOwnList = document.getElementById('chooseOthersList');
 const body = document.body;
 
 // Create a function to change the background color
 function changeBackgroundColor() {
   // Define an array of colors
-  const colors = ['#FF5733', '#33FF57', '#3357FF', '#FFFF33', '#FF33FF'];
+  const colors = ['#191618', '#d6002a', '#346052', '#237f79', '#dadbed'];
   
   // Pick a random color from the array
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
@@ -16,3 +18,5 @@ function changeBackgroundColor() {
 
 // Add an event listener to the button to call the function when clicked
 changeColorBtn.addEventListener('click', changeBackgroundColor);
+chooseOwnList.addEventListener('click', changeBackgroundColor);
+chooseOthersList.addEventListener('click', changeBackgroundColor);
